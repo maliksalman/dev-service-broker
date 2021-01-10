2e8d4ac574d0da0d35c138d532da42f1370f4792
Salman Malik <salman@the-tech-warriors.com>
2021-01-10 01:00:05 -0500

    Adding some debug info

--- src/main/java/com/smalik/devservicebroker/provisioner/ProcessRunner.java
+++ src/main/java/com/smalik/devservicebroker/provisioner/ProcessRunner.java
@@ -1,7 +1,10 @@
 package com.smalik.devservicebroker.provisioner;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import java.awt.desktop.AboutEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
@@ -9,10 +12,14 @@ import java.io.InputStreamReader;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
+@Slf4j
 @Service
 public class ProcessRunner {
  
   public int runProcess(String... args) throws Exception {
+
+    log.debug("Running: [[" + StringUtils.join(args, ' ') + "]]");
+
     ProcessBuilder builder = new ProcessBuilder();
     builder.command(args);
     builder.directory(new File(System.getProperty("user.home")));
